265d7e971aa28601244f33ed1f123b1f33124561
BrettG <2004brett@gmail.com>
2022-10-14 16:24:10 -0500

    Edited js

--- assets/script.js
+++ assets/script.js
@@ -15,8 +15,7 @@ $('.saveBtn').on('click', function () {
   localStorage.setItem(timeKey, textValue);
 });
 
-// Get item from local storage if any
-$('#hour8 .description').val(localStorage.getItem('hour8'));
+
 $('#hour9 .description').val(localStorage.getItem('hour9'));
 $('#hour10 .description').val(localStorage.getItem('hour10'));
 $('#hour11 .description').val(localStorage.getItem('hour11'));
@@ -27,24 +26,25 @@ $('#hour15 .description').val(localStorage.getItem('hour15'));
 $('#hour16 .description').val(localStorage.getItem('hour16'));
 $('#hour17 .description').val(localStorage.getItem('hour17'));
 
-// Function to track tasks and make them change colors if they are in the past, present or future
+
+
 function auditTask() {
-  // get current number of hours
+  
   var currentHour = today.hours();
 
-  // loop over each time block
+  
   $('.time-block').each(function () {
     var timeId = parseInt($(this).attr('id').split("hour")[1]);
 
-    // if the time Id attribute is before the current hour, add the past class
+   
     if (timeId < currentHour) {
       $(this).addClass('past');
-    } // if the time Id attribute is equal to the current hour, remove the past and future classes and add the present class
+    }
     else if (timeId === currentHour) {
       $(this).removeClass('past');
       $(this).removeClass('future');
       $(this).addClass('present');
-    } // If the time Id attribute is greater than the current time, remove the past and present classes and add the future class
+    }
     else {
       $(this).removeClass('past');
       $(this).removeClass('present');
@@ -53,11 +53,11 @@ function auditTask() {
   });
 }
 
-// Call the audit task function
+
 auditTask();
 
-// Use setTimeout to update the time every minute (1000ms * 60s)
+
 setTimeout(function () {
-  // clear the current URL
-  location = ''; // location references the current URL
+
+  location = ''; 
 }, 1000 * 60);
